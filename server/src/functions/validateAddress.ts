548cdae82c7d86469e0ea3e245d79f690c0e23ea
Kevin <kevin.gervais@tutanota.com>
2023-02-02 15:05:25 -0500

    .

--- server/src/functions/validateAddress.ts
+++ server/src/functions/validateAddress.ts
@@ -1,12 +1,12 @@
 import { AllCountryCodes, AllLanguages, PostalAddress, addressToString, generateId } from "shared"
 
-import { DbQueries } from "@/classes"
+import { Mongodb } from "@/classes"
 import { HERE_API_KEY } from "@/constants"
 
 import { getSay } from "./getSay"
 import { throwError } from "./throwError"
 
-export async function validateAddress(address: PostalAddress, language: AllLanguages, dbQueries: DbQueries): Promise<PostalAddress> | never {
+export async function validateAddress(address: PostalAddress, language: AllLanguages, mongodb: Mongodb): Promise<PostalAddress> | never {
   const nodeFetch = (url: string, init?: RequestInit) => import("node-fetch").then(result => result.default(url, init as any))
   address.address = addressToString(address)
   const query = `https://geocode.search.hereapi.com/v1/geocode?apiKey=${HERE_API_KEY}&q=${encodeURIComponent(address.address)}&lang=${language}-CA&in=countryCode:CAN&limit=1`
@@ -18,11 +18,11 @@ export async function validateAddress(address: PostalAddress, language: AllLangu
   }
   address.lat = item.position.lat
   address.lng = item.position.lng
-  const existingAddress = await dbQueries.getOne("addresses", { _id: address._id })
+  const existingAddress = await mongodb.getOne("addresses", { _id: address._id })
   if (existingAddress && existingAddress.address !== address.address) {
     address._id = generateId()
   }
-  const say = getSay("fr")
+  const say = getSay(language)
   const country = say[(item.address.countryCode as string).toLowerCase() as AllCountryCodes]
   if (country === undefined || country !== "canada") {
     throwError("invalidLocation")
