39f5f858f872ce2e7a3505f5929e9718d36f9c55
Kevin <kevin.gervais@tutanota.com>
2023-02-02 16:06:53 -0500

    .

--- server/src/functions/validateAddress.ts
+++ server/src/functions/validateAddress.ts
@@ -1,19 +1,16 @@
 import { AllCountryCodes, AllLanguages, PostalAddress, addressToString, generateId } from "shared"
 
 import { Mongodb } from "@/classes"
-import { HERE_API_KEY } from "@/constants"
 
-import { getSay } from "./getSay"
+import { handleHereRequest } from "./handleHereRequest"
 import { throwError } from "./throwError"
 
 export async function validateAddress(address: PostalAddress, language: AllLanguages, mongodb: Mongodb): Promise<PostalAddress> | never {
-  const nodeFetch = (url: string, init?: RequestInit) => import("node-fetch").then(result => result.default(url, init as any))
   address.address = addressToString(address)
-  const query = `https://geocode.search.hereapi.com/v1/geocode?apiKey=${HERE_API_KEY}&q=${encodeURIComponent(address.address)}&lang=${language}-CA&in=countryCode:CAN&limit=1`
-  const response = await nodeFetch(query)
-  const json: any = await response.json()
-  const item = json.items[0]
+  const response = await handleHereRequest("geocode", { searchField: address.address, language })
+  const item = (response.items || [])[0]
   if (!item || item.resultType !== "houseNumber") {
+    console.log(item?.resultType)
     throwError("invalidLocation")
   }
   address.lat = item.position.lat
@@ -22,9 +19,8 @@ export async function validateAddress(address: PostalAddress, language: AllLangu
   if (existingAddress && existingAddress.address !== address.address) {
     address._id = generateId()
   }
-  const say = getSay(language)
-  const country = say[(item.address.countryCode as string).toLowerCase() as AllCountryCodes]
-  if (country === undefined || country !== "canada") {
+  const countryCode = (item.address.countryCode as string).toLowerCase() as AllCountryCodes
+  if (countryCode === undefined || countryCode !== "can") {
     throwError("invalidLocation")
   }
   return address as PostalAddress
